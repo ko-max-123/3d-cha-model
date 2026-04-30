a68517144638b6272c73758e239be7d6c08037ae
T_koyama <88242174+ko-max-123@users.noreply.github.com>
2026-04-30 16:17:09 +0000

    20260501

--- model-answers.js
+++ model-answers.js
@@ -1,6 +1,6 @@
 window.PHON_APP_MODEL_ANSWERS = {
   // 正誤判定に使う文字列です。GLBを差し替えた場合はここだけ変更してください。
-  model0: 'RQLWFT',
+  model0: 'RSCHFM',
   model1: 'WSLTRF',
   model2: 'PHEKSV',
   model3: 'CULHOX'
